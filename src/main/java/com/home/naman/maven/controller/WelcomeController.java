061a5df8a654847255db3807e8ac5c86f7044b21
naman.chopra <naman.chopra@puresoftware.com>
2020-07-12 15:11:56 +0530

    Refactoring code and adding lombok for boiler-plate code

--- src/main/java/com/home/naman/maven/controller/WelcomeController.java
+++ src/main/java/com/home/naman/maven/controller/WelcomeController.java
@@ -1,19 +1,16 @@
 /**
- * 
+ *
  */
 package com.home.naman.maven.controller;
 
-import java.util.Map;
-
-import javax.websocket.server.PathParam;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.home.naman.maven.model.Person;
 import com.home.naman.maven.service.WelcomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.util.Map;
 
 /**
  * @author naman
@@ -21,26 +18,26 @@ import com.home.naman.maven.service.WelcomeService;
  */
 @RestController
 public class WelcomeController {
-	
-	@Autowired
-	private WelcomeService service;
-	
-	@RequestMapping(value="/welcome")
-	public Map<String,Person> welcome(){
-		return service.welcomeMessage();
-	}
-	
-	@RequestMapping("/getNext")
-	public Map<String,Person> getNext(){
-		return service.getNext();
-	}
-	
-	@RequestMapping("/dynamic")
-	public Map<String,Person> getDynamic(@PathParam("var") String var){
-		if("1".equals(var))
-		    return service.getNext();
-		else
-			return service.welcomeMessage();
-	}
+
+    @Autowired
+    private WelcomeService service;
+
+    @GetMapping(value = "/welcome")
+    public Map<String, Person> welcome() {
+        return service.welcomeMessage();
+    }
+
+    @GetMapping("/getNext")
+    public Map<String, Person> getNext() {
+        return service.getNext();
+    }
+
+    @GetMapping("/dynamic")
+    public Map<String, Person> getDynamic(@PathParam("var") String var) {
+        if ("1".equals(var))
+            return service.getNext();
+        else
+            return service.welcomeMessage();
+    }
 
 }
